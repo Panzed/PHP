5f50dcb7776c8b7edaaf83c4dcf89600902de825
Matteo Pasquinoni <matteopasquinoni@gmail.com>
2025-02-14 12:49:35 +0100

    lezione2-Chiusoli

--- ChiusoliPHP/Lezione1/esempio.php
+++ ChiusoliPHP/Lezione1/esempio.php
@@ -13,16 +13,24 @@
     <p>Ciao</p>
 
     <?php
-    echo "prima frase in php <br> Hello world!";
+    echo 'prima frase in php <br> Hello world!';  // <br> serve per andare a capo all'interno del della stringa php
+
+    echo ' <br> Gani dice l\'informazione: "Oggi piove!" '; // aggiungendo \ si può usare il carattere speciale
+
+    echo " <br> Gani dice l'informazione: ";   // con due righe diverse si può scrivere in modo più leggibile
+    echo '"Oggi piove!"';
+
+    echo " <br> Gani dice l'informazione: " . '"Oggi piove!"'; // concatenando le stringhe si può scrivere in modo più leggibile
     ?>
 
     <br>
 
-    <?php
-    echo "Data di oggi: ";
-    echo date("<b> d/m/Y h:i:s </b>");
-    ?>
-
+    <div>
+        <?php
+        echo "Data di oggi: ";
+        echo date("<b> d/m/Y h:i:s </b>"); // echo date("d/m/Y h:i:s"); // il tag html <b> sempre dentro le virgolette
+        ?>
+    </div>
 </body>
 
 </html>
