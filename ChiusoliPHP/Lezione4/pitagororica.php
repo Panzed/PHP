7db863f92176a610ceb622a60d79b4101522c83e
Matteo Pasquinoni <matteopasquinoni@gmail.com>
2025-02-26 17:50:50 +0100

    nuova tavola pitagorica

--- ChiusoliPHP/Lezione4/pitagororica.php
+++ ChiusoliPHP/Lezione4/pitagororica.php
@@ -10,26 +10,25 @@
 <body>
     <h1>Tavola Pitagorica</h1> 
 
-    <table style="border: 1px solid black">
-        <tr>
-            <th></th>    <!-- cella vuota -->
-            <?php
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<th>$i</th>";
-            }
-            ?>
-        </tr>
+<table>
+    <tr>
+        <th></th>
         <?php
-        for ($i = 1; $i <= 10; $i++) {
-            echo "<tr>";
-            echo "<th>$i</th>";
-            for ($j = 1; $j <= 10; $j++) {
-                echo "<td>" . $i * $j . "</td>";
-            }
-            echo "</tr>";
-        }
+        for ($i=1; $i<=10; $i++)
+        echo "<th>$i</th>";
         ?>
-    </table>
+    </tr>
+    <?php
+    for ($i=1; $i<=10; $i++){
+        echo "<tr>";
+        echo "<th> $i </th>";
+        for ($y=1; $y<=10; $y++){
+            echo "<td>" . $i * $y . "</td>";
+        } 
+        echo "</tr>";
+    }
+    ?>
+</table>
 
 </body>
 
