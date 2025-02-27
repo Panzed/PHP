63d137d8ac85011343e6dc9a44f3bf1a34be1156
Matteo Pasquinoni <matteopasquinoni@gmail.com>
2025-02-27 16:12:57 +0100

    Lezione 5

--- ChiusoliPHP/Lezione4/pitagororica.php
+++ ChiusoliPHP/Lezione4/pitagororica.php
@@ -8,27 +8,54 @@
 </head>
 
 <body>
-    <h1>Tavola Pitagorica</h1> 
+    <h1>Tavola Pitagorica</h1>
 
-<table>
-    <tr>
-        <th></th>
+    <table>
+        <tr>
+            <th></th>
+            <?php
+            for ($i = 1; $i <= 10; $i++)
+                echo "<th>$i</th>";
+            ?>
+        </tr>
         <?php
-        for ($i=1; $i<=10; $i++)
-        echo "<th>$i</th>";
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<tr>";
+            echo "<th> $i </th>";
+            for ($y = 1; $y <= 10; $y++) {
+                echo "<td>" . $i * $y . "</td>";
+            }
+            echo "</tr>";
+        }
         ?>
-    </tr>
-    <?php
-    for ($i=1; $i<=10; $i++){
-        echo "<tr>";
-        echo "<th> $i </th>";
-        for ($y=1; $y<=10; $y++){
-            echo "<td>" . $i * $y . "</td>";
-        } 
-        echo "</tr>";
-    }
-    ?>
-</table>
+    </table>
+
+    <br><br><br><br><br>
+
+
+
+    <table style="border: 1px solid black;">
+        <tr>
+            <th></th>
+            <?php
+
+            for ($i = 1; $i <= 10; $i++)
+                echo "<th> $i </th>";
+            ?>
+        </tr>
+
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<tr>";
+            echo "<th> $i </th>";
+            for ($y = 1; $y <= 10; $y++) {
+                echo "<td>" . $y * $i . "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
 
 </body>
 
