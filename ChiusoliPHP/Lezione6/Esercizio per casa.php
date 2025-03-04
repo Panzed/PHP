ca5637eee1c29381c850fb2ce74555d0e8711b4c
Matteo Pasquinoni <matteopasquinoni@gmail.com>
2025-03-04 13:02:09 +0100

    Lezione 6

---
+++ ChiusoliPHP/Lezione6/Esercizio per casa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Viene fornito un array di interi, dove ogni numero rappresenta l&#39;età degli abitanti di un piccolo
+    comune. Si chiede di svolgere le seguenti attività:
+    <br>1. elencare tutte le età presenti nell&#39;array con uno sfondo alternato nelle varie righe,
+    scrivendo in grassetto le età dei maggiorenni e in corsivo quelle dei minorenni (0
+    corrisponde ad un neonato);
+    <br>2. in un riquadro, riportare il numero dei neonati e l&#39;età del più anziano;
+    <br>3. calcolare e visualizzare l&#39;età media dei maggiorenni.
+    <br>4. Per aver diritto al voto si deve avere almeno 18 anni: calcolare e visualizzare quante
+    schede servono in totale per le elezioni.
+</body>
+
+</html>
