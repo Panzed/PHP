f805469c9778796ae4ad9fcbef2c1a809446b0ca
Matteo Pasquinoni <matteopasquinoni@gmail.com>
2025-03-11 18:43:50 +0100

    verifica copy

--- ChiusoliPHP/Verifica/Verifica-copy.php
+++ ChiusoliPHP/Verifica/Verifica-copy.php
@@ -6,90 +6,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-
-
     <?php
 
-    $tot_studenti = rand(20, 50);
-    $voto_partecipanti = [];
-    $lode = $promossi = $somma = 0;
-    $min_voto = 101;
-
-
-
-    for ($i = 0; $i < ($tot_studenti - 2); $i++) {
-        $voto_partecipanti[] = rand(1, 101);
-    }
+    $n_partecipanti = rand(20, 50);
+    $voti = [];
 
     for ($i = 1; $i <= 101; $i++) {
         $a[$i] = 0;
     }
 
+    for ($i = 1; $i <= ($n_partecipanti - 2); $i++) {
+        $voti[] = rand(1, 101);
+    }
+
+    $lode = 0;
+    $promossi = $somma = 0;
+    $voto_min = 110;
+    $n_voto_min = 0;
+
     echo "<ol>";
-    foreach ($voto_partecipanti as $key => $value) {
 
-        if ($key % 2 == 0) {
-            $colore = 'yellow';
-        } else {
-            $colore = 'pink';
+    foreach ($voti as $i => $voto) {
+        $colore = "pink";
+
+        if ($i % 2 == 0) {
+            $colore = "yellow";
         }
 
-        $volte_voto = $a[$value];
-        $a[$value] = ++$volte_voto;
-
-        if ($value < $min_voto) {
-            $min_voto = $value;
-            $n_min_voto = 0;
-        }
-
-        if ($value == $min_voto) {
-            $n_min_voto++;
-        }
-
-        if ($value == 101) {
+        if ($voto == 101) {
             $lode++;
         }
 
-        if ($value >= 60) {
-            $promossi++;
-            $somma += $value;
-            $value = "<b> $value </b>";
-        } else {
-            $value = "<i> $value </i>";
+        if ($voto < $voto_min) {
+            $voto_min = $voto;
+            $n_voto_min = 0;
         }
 
+        if ($voto == $voto_min) {
+            $n_voto_min++;
+        }
 
+        $volte_voto = $a[$voto];
+        $a[$voto] = $volte_voto + 1;
 
-
-
-        echo "<li><span style='background-color: $colore'> $value </span></li>";
+        if ($voto >= 60) {
+            $promossi++;
+            $somma += $voto;
+            $voto = "<b> $voto </b>";
+        } else {
+            $voto = "<i> $voto </i>";
+        }
+        echo "<li><span style='background-color: $colore'> $voto </span></li>";
     }
     echo "</ol>";
 
-
-    echo "Sono state assegnate n. $lode lodi <br>";
-    echo "Il voto medio dei promossi è di " . $somma / $promossi . "<br>";
-    echo "Il voto più basso è stato di $min_voto ed è uscito n. $n_min_voto volte";
-
-
-    echo "<hr>";
+    echo "Sono state assegnate n. $lode lodi. <br>";
+    echo "La media tra i voti dei promossi è di " . $somma / $promossi . "<br>";
+    echo "Il voto minimo è $voto_min e si ripete n. $n_voto_min volte <br>";
 
 
     echo "<table>";
-    foreach ($a as $key => $value) {
-        echo "<tr><td> $key => $value </td></tr>";
+    foreach ($a as $k => $v) {
+        echo "<tr>";
+        echo "<td>$k => $v</td>";
+        echo "</tr>";
     }
     echo "</table>";
 
-
-
+    print_r($a);
     ?>
-
-
-
-
 
 </body>
 
